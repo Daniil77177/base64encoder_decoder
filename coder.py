9b8c937660304c5cc6504149f457e7a6a4ba17bd
Daniil77177 <Daniil200277177@gmail.com>
2019-06-16 15:55:58 +0300

    Programm is working perfectly

--- coder.py
+++ coder.py
@@ -1,8 +1,8 @@
 import random, base64
-def encode (input):
-    base64variable = base64.b64encode(input)
+def encode (input1):
+    base64variable = base64.b64encode(bytes(input1,'utf-8'))
     print(base64variable)
 
-def decode (input):
-    base64variable = base64.b64decode(input)
+def decode (input1):
+    base64variable = base64.b64decode(bytes(input1,'utf-8'))
     print(str(base64variable))
